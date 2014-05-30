37e930d0667b56007f336aebcae5189d40cb1a8f
Caleb James DeLisle <cjd@hyperboria.ca>
2014-05-30 14:57:19 +0200

    Change the version number to 1.1

--- src/xwiki/this.js
+++ src/xwiki/this.js
@@ -2,5 +2,5 @@ Package(function (pack, XWiki) {
     pack.setName("Realtime Wiki Editor");
     pack.setDescription("Collaboratively edit your XWiki Documents with others");
     pack.setExtensionId("org.xwiki.contrib:xwiki-contrib-rtwiki");
-    pack.setXarName("realtime-wiki-1.0.xar")
+    pack.setXarName("realtime-wiki-1.1.xar")
 });
