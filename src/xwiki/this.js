59dc3ac144de1b77f4ab844ed8538a4bb5abb18b
ansuz <ansuz@transitiontech.ca>
2015-12-21 12:05:28 -0500

    leave HEAD in SNAPSHOT state

--- src/xwiki/this.js
+++ src/xwiki/this.js
@@ -2,5 +2,5 @@ Package(function (pack, XWiki) {
     pack.setName("Realtime Wiki Editor");
     pack.setDescription("Collaboratively edit your XWiki Documents with others");
     pack.setExtensionId("org.xwiki.contrib:xwiki-contrib-rtwiki");
-    pack.setXarName("realtime-wikitext-1.13.xar")
+    pack.setXarName("realtime-wikitext-1.14-SNAPSHOT.xar")
 });
